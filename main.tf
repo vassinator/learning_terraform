76b8029d04b38d0fd632c5a46df91f210186e1b3
vassinator <35261051+vassinator@users.noreply.github.com>
2026-02-19 10:42:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "blog" 
+resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
