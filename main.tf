d2c4154421e37ae0357187dfe75dfa4f948df257
vassinator <hazvass@gmail.com>
2026-02-20 12:58:08 +0000

    Autoscaling

--- main.tf
+++ main.tf
@@ -32,17 +32,6 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -96,3 +85,25 @@ resource "aws_lb_target_group_attachment" "blog" {
   target_id        = aws_instance.blog.id
   port             = 80
 }
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+
+  security_groups        = [module.blog_sg.security_group_id]
+  instance_type          = var.instance_type
+  image_id               = data.aws_ami.app_ami.id
+
+  traffic_source_attachments = {
+    blog-abl = {
+      traffic_source_identifier = aws_lb_target_group.blog.arn
+    }
+  }
+}
+
