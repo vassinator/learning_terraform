8f7c9ee7e12d4ad607082771e7f390bf7112cd15
vassinator <hazvass@gmail.com>
2026-02-20 13:05:23 +0000

    Autoscaling

--- main.tf
+++ main.tf
@@ -80,6 +80,7 @@ resource "aws_lb_target_group" "blog" {
   vpc_id   = module.blog_vpc.vpc_id
 }
 
+module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.2.0"
 
