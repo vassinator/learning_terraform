3f81f98c88989a749e30c4d5de88d6d72cbed30e
vassinator <hazvass@gmail.com>
2026-02-20 12:12:25 +0000

    Load Balancer

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ module "blog_sg" {
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name    = "blog_alb"
+  name    = "blog-alb"
   vpc_id  = "module.blog_vpc.vpc_id"
   subnets = module.blog_vpc.public_subnets
 
