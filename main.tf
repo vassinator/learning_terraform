010677b049dc4abe1480917484396d10aa4653f6
vassinator <hazvass@gmail.com>
2026-02-20 12:23:19 +0000

    Load Balancer

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "aws_lb_target_group" "blog" {
   name     = "blog"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = module.blog_vpc.vpc_id
+  vpc_id   = "module.blog_vpc.vpc_id"
 }
 
 resource "aws_lb_target_group_attachment" "blog" {
