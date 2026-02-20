e960e6d7602b6119e6cb44fbd31036fb48f828af
vassinator <hazvass@gmail.com>
2026-02-20 13:03:49 +0000

    Autoscaling

--- main.tf
+++ main.tf
@@ -80,13 +80,6 @@ resource "aws_lb_target_group" "blog" {
   vpc_id   = module.blog_vpc.vpc_id
 }
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
-}
-
-module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.2.0"
 
